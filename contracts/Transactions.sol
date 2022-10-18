b5e621585b28372662b6ecd1a68a8d75914dd54e
tunahan dündar <84476929+immpeccable@users.noreply.github.com>
2022-10-18 10:32:40 +0300

    compiler options fixed

--- contracts/Transactions.sol
+++ contracts/Transactions.sol
@@ -1,35 +1,67 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.9;
 
 contract Transactions {
     uint256 transactionCount;
 
-    event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
+    event Transfer(
+        address from,
+        address receiver,
+        uint256 amount,
+        string message,
+        uint256 timestamp,
+        string keyword
+    );
 
     struct TransferStruct {
         address sender;
         address receiver;
-        uint amount;
+        uint256 amount;
         string message;
         uint256 timestamp;
-        string keyword;   
+        string keyword;
     }
 
     TransferStruct[] transactions;
-    function addToBlockchain(address receiver, uint amount, string memory message, string memory keyword) public {
-        transactionCount++;
-        transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
 
-        emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
+    function addToBlockchain(
+        address receiver,
+        uint256 amount,
+        string memory message,
+        string memory keyword
+    ) public {
+        transactionCount++;
+        transactions.push(
+            TransferStruct(
+                msg.sender,
+                receiver,
+                amount,
+                message,
+                block.timestamp,
+                keyword
+            )
+        );
+
+        emit Transfer(
+            msg.sender,
+            receiver,
+            amount,
+            message,
+            block.timestamp,
+            keyword
+        );
     }
 
-    function getAllTransactions() public view returns (TransferStruct[] memory) {
+    function getAllTransactions()
+        public
+        view
+        returns (TransferStruct[] memory)
+    {
         return transactions;
     }
 
-    function getTransactionCount() public view returns (uint) {
+    function getTransactionCount() public view returns (uint256) {
         return transactionCount;
     }
-
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,7 +4,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: "0.8.9",
   networks: {
     goerli: {
       url: "https://eth-goerli.g.alchemy.com/v2/UwleiIj3_exLYoRpJrbcMyEeJkDdKhZF",
